b3cef1e61d81e29bbb82d9b4c931a05b023d79ca
admin <tabishskhn2gmail.com>
2020-03-04 04:30:21 -0800

    Tabish

--- app/src/imuons/cpp/main.cpp
+++ app/src/imuons/cpp/main.cpp
@@ -5,7 +5,8 @@ extern "C" {
 
 std::string getDomain(
         JNIEnv *env) {
-     std::string data = "http://139.162.28.58/";
+    // std::string data = "http://139.162.28.58/";
+     std::string data = "http://172.105.48.28/";
     return data;
 
 }
@@ -13,14 +14,16 @@ std::string getDomain(
 
 std::string getBaseUrl(
         JNIEnv *env) {
-    std::string data = getDomain(env) + "imuons/admin/apis/mobile/v1/";
+   // std::string data = getDomain(env) + "imuons/admin/apis/mobile/v1/";
+    std::string data = getDomain(env) + "admin/apis/mobile/v1/";
     return data;
 }
 
 
 std::string getBasePageUrl(
         JNIEnv *env) {
-    std::string data = getDomain(env) + "imuons/admin/m/pages/";
+ //   std::string data = getDomain(env) + "imuons/admin/m/pages/";
+    std::string data = getDomain(env) + "admin/m/pages/";
     return data;
 }
 
