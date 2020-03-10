41f584717b8b7b9618367e07ae5a319ba27afd16
admin <tabishskhn2gmail.com>
2020-03-10 02:38:06 -0700

    Tabish

--- app/src/imuons/cpp/main.cpp
+++ app/src/imuons/cpp/main.cpp
@@ -6,7 +6,11 @@ extern "C" {
 std::string getDomain(
         JNIEnv *env) {
    // std::string data = "http://139.162.28.58/";
-     std::string data = "http://172.105.48.28/";
+     //std::string data = "http://172.105.48.28/"; // st11
+     std::string data = "http://172.105.49.123/";    //MyST11
+
+
+
     return data;
 
 }
